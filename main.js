5cebaea2204c4a73ca031f718adaf950a1c4f47e
Wilmerd0 <sowx.diaz@gmail.com>
2024-04-20 23:43:21 -0400

    add funtions and querySelectos

---
+++ main.js
@@ -0,0 +1,40 @@
+function playSonidoPom () {
+    document.querySelector('#sonido_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
+
+
+
+
+
+
+
+// function playSonidoTim (){
+//     document.querySelector('#sonido_tecla_tim').play;
+// };
+// function playSonidoPuff (){
+//     document.querySelector('#sonido_tecla_puff').play;
+// };
+// function playSonidoSplash (){
+//     document.querySelector('#sonido_tecla_splash').play;
+// };
+// function playSonidoClap (){
+//     document.querySelector('#sonido_tecla_clap').play;
+// };
+// function playSonidoToim (){
+//     document.querySelector('#sonido_tecla_toim').play;
+// };
+// function playSonidoPsh (){
+//     document.querySelector('#sonido_tecla_psh').play;
+// };
+// function playSonidoTic (){
+//     document.querySelector('#sonido_tecla_tic').play;
+// };
+// function playSonidoTom (){
+//     document.querySelector('#sonido_tecla_tom').play;
+// };
+
+
+
