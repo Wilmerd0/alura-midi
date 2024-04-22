f0b12762f97bf8651028bc01ef101f0626803257
Wilmerd0 <sowx.diaz@gmail.com>
2024-04-22 12:46:23 -0400

    improve for loop

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 
-for (let contador = 0; contador > 9; contador++){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio);
     tecla.onclick =function (){
         playSonido(idAudio);
     };
-}
+};
 
 
 
